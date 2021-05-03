d7b018887c5524aba22b5afd87d1cbef72496944
Mich0608 <mjtchen68@gmail.com>
2021-05-03 00:31:28 -0500

    implemented waves

--- 10print.js
+++ 10print.js
@@ -4,14 +4,23 @@
 
 const w = process.stdout.columns
 
+const waves = ['*･゜ﾟ･*:.｡..｡.:*･', '*･゜ﾟ･*:.｡..｡.:*･', '🛥']
+const colors = ['\x1b[91m', '\x1b[92m', '\x1b[93m', '\x1b[94m', '\x1b[95m']
+
 function draw () {
-  setTimeout(draw, 1000)
+  setTimeout(draw, 1000 / 4)
   let output = ''
   for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
+    let r = Math.floor(Math.random() * waves.length)
+    if (Math.random() > 0.2) {
+      let c = Math.floor(Math.random() * colors.length)
+      output += colors[c]
+    }
+    else if (r == 1 | r == 2) {
+      output += waves[r] + '\x1b[34m'
+    }
+    else {
+      output += waves[r]
     }
   }
   console.log(output)
